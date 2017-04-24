05ae483d32bb93dc834f95d1a8bdd8645ce0d440
Antoine Nguyen <tonio@ngyn.org>
2017-04-24 15:42:32 +0200

    Provide default settings from one place.
    fix #2

--- modoboa_contacts/settings.py
+++ modoboa_contacts/settings.py
@@ -10,3 +10,17 @@ CONTACTS_STATS_FILES = {
     "prod": os.path.join(
         PLUGIN_BASE_DIR, "static/modoboa_contacts/webpack-stats.json")
 }
+
+
+def apply(settings):
+    """Modify settings."""
+    DEBUG = settings['DEBUG']
+    settings["INSTALLED_APPS"] += ("webpack_loader", )
+    settings["WEBPACK_LOADER"] = {
+        'CONTACTS': {
+            'CACHE': not DEBUG,
+            'BUNDLE_DIR_NAME': 'modoboa_contacts/',
+            'STATS_FILE': CONTACTS_STATS_FILES.get("dev" if DEBUG else "prod"),
+            'IGNORE': ['.+\.hot-update.js', '.+\.map']
+        }
+    }
