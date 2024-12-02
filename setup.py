7e3d8263b6f639dd647fa8fdc04487eefbc43962
Antoine Nguyen <tonio@ngyn.org>
2024-12-02 16:37:06 +0100

    Updated packaging

--- setup.py
+++ setup.py
@@ -8,32 +8,9 @@ See:
 https://packaging.python.org/en/latest/distributing.html
 """
 
-import io
 from os import path
 
-try:
-    from pip.req import parse_requirements
-except ImportError:
-    # pip >= 10
-    from pip._internal.req import parse_requirements
-
 from setuptools import setup, find_packages
-
-
-def get_requirements(requirements_file):
-    """Use pip to parse requirements file."""
-    requirements = []
-    if path.isfile(requirements_file):
-        for req in parse_requirements(requirements_file, session="hack"):
-            try:
-                if req.markers:
-                    requirements.append("%s;%s" % (req.req, req.markers))
-                else:
-                    requirements.append("%s" % req.req)
-            except AttributeError:
-                # pip >= 20.0.2
-                requirements.append(req.requirement)
-    return requirements
 
 
 def local_scheme(version):
@@ -46,39 +23,14 @@ def local_scheme(version):
 
 if __name__ == "__main__":
     HERE = path.abspath(path.dirname(__file__))
-    INSTALL_REQUIRES = get_requirements(path.join(HERE, "requirements.txt"))
 
-    with io.open(path.join(HERE, "README.rst"), encoding="utf-8") as readme:
+    with open(path.join(HERE, "README.rst"), encoding="utf-8") as readme:
         LONG_DESCRIPTION = readme.read()
 
     setup(
-        name="modoboa-contacts",
-        description="Address book for Modoboa",
         long_description=LONG_DESCRIPTION,
-        license="MIT",
-        url="http://modoboa.org/",
-        author="Antoine Nguyen",
-        author_email="tonio@ngyn.org",
-        classifiers=[
-            "Development Status :: 5 - Production/Stable",
-            "Environment :: Web Environment",
-            "Framework :: Django :: 2.2",
-            "Intended Audience :: System Administrators",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.9",
-            "Programming Language :: Python :: 3.10",
-            "Programming Language :: Python :: 3.11",
-            "Programming Language :: Python :: 3.12",
-            "Topic :: Communications :: Email",
-            "Topic :: Internet :: WWW/HTTP",
-        ],
-        keywords="email",
         packages=find_packages(exclude=["test_project"]),
         include_package_data=True,
         zip_safe=False,
-        install_requires=INSTALL_REQUIRES,
         use_scm_version={"local_scheme": local_scheme},
-        setup_requires=["setuptools_scm"],
     )
